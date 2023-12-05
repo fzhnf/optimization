d904e5077887ae57443c690714cd6b38668f9b77
fzhnf <faizahanf52@gmail.com>
2023-12-05 19:40:07 +0800

    print perhitungan

--- djikstra.py
+++ djikstra.py
@@ -2,13 +2,13 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 class Node:
-    def __init__(self, name, neighbors):
+    def __init__(self, name, neighbors)->None:
         self.name = name
         self.neighbors = neighbors
 
-class Dijkstra:
-    def __init__(self, graph):
-        # Inisialisasi objek Dijkstra dengan graf sebagai parameter
+class Djikstra:
+    def __init__(self, graph)->None:
+        # Inisialisasi objek djikstra dengan graf sebagai parameter
         self.graph = graph
         # Membuat objek Node untuk setiap node dalam graf
         for name, neighbors in graph.items():
@@ -17,28 +17,39 @@ class Dijkstra:
     def find_shortest_path(self, start_node, end_node):
         # Inisialisasi jarak ke setiap node sebagai tak terhingga
         distance = {node: float('inf') for node in self.graph}
+        print(f"Initial distance: {distance}")
+        
         # Inisialisasi node pendahulu ke setiap node sebagai None
         predecessor = {node: None for node in self.graph}
+        print(f"Initial predecessors: {predecessor}")
+        
         # Jarak dari start_node ke dirinya sendiri diatur sebagai 0
         distance[start_node] = 0
+        print(f"Set distance of {start_node} to 0: {distance}")
+        
         # Antrian untuk menyimpan pasangan jarak-node yang akan dieksplorasi
         queue = [(0, start_node)]
+        print(f"Initial queue: {queue}")
         
         # Algoritma Dijkstra
         while queue:
             current_distance, current_node = min(queue)
             queue.remove((current_distance, current_node))
-
+            print(f"\nExploring {current_node} with distance {current_distance}")
+            
             for neighbor, weight in self.graph[current_node].neighbors.items():
                 new_distance = current_distance + weight
                 if new_distance < distance[neighbor]:
                     distance[neighbor] = new_distance
                     predecessor[neighbor] = current_node
                     queue.append((new_distance, neighbor))
+                    print(f"Updated distance to {neighbor}: {distance}")
+                    print(f"Updated predecessor of {neighbor}: {predecessor}")
 
         # Mendapatkan jarak terpendek dari start_node ke end_node
         shortest_distance = distance[end_node]
         if shortest_distance == float('inf'):
+            print("No path found.")
             return None  # Tidak ada jalur yang ditemukan
 
         # Membuat jalur dari end_node ke start_node
@@ -46,7 +57,12 @@ class Dijkstra:
         while path[-1] != start_node:
             path.append(predecessor[path[-1]])
 
+        print(f"\nShortest path from {start_node} to {end_node}: {path[::-1]}")
         return path[::-1]
+
+
+
+
 
 def visualize_graph_with_highlighted_path(graph, shortest_path):
     # Membuat objek graf dari NetworkX
@@ -59,14 +75,13 @@ def visualize_graph_with_highlighted_path(graph, shortest_path):
     # Menentukan posisi node dalam graf
     pos = nx.spring_layout(G)
     # Menggambar graf dengan label node, warna, dan ukuran node
-    nx.draw(G, pos, with_labels=True, node_size=720, node_color='#acb0be', font_size=15)
+    nx.draw(G, pos, with_labels=True, node_size=800, node_color='#acb0be', font_size=15)
 
     if shortest_path:
         # Jika ada jalur terpendek, visualisasikan dengan warna yang berbeda
         path_edges = [(shortest_path[i], shortest_path[i + 1]) for i in range(len(shortest_path) - 1)]
         nx.draw_networkx_nodes(G, pos, nodelist=shortest_path, node_color='#f38ba8', node_size=960)
-        nx.draw_networkx_edges(G, pos, edgelist=path_edges, edge_color='#eba0ac', width=2.5,arrows=True ,arrowstyle='-|>' )
-
+        nx.draw_networkx_edges(G, pos, edgelist=path_edges, edge_color='#eba0ac', arrowstyle='-|>')
         # Menambahkan label berat pada setiap edge
         labels = nx.get_edge_attributes(G, 'weight')
         nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
@@ -76,7 +91,7 @@ def visualize_graph_with_highlighted_path(graph, shortest_path):
 
 def main():
     # Definisi graf
-    graph = {
+    graph: dict[str,dict[str,int]]= {
         'A': {'B':16, 'C':3, 'D':10},
         'B': {'D':2,'I':8},
         'C': {'B':12,'E':7},
@@ -87,17 +102,23 @@ def main():
         'I': {},
     }
     # Node awal dan akhir untuk mencari jalur terpendek
-    start_node = 'A'
-    end_node = 'I'
+    start_node:str = 'A'
+    end_node:str = 'I'
 
-    # Membuat objek Dijkstra
-    dijkstra_algorithm = Dijkstra(graph)
+    # Membuat objek djikstra
+    djikstra:Djikstra = Djikstra(graph)
     # Mencari jalur terpendek dari start_node ke end_node
-    shortest_path = dijkstra_algorithm.find_shortest_path(start_node, end_node)
+    shortest_path = djikstra.find_shortest_path(start_node, end_node)
 
     # Menampilkan hasil jalur terpendek
     if shortest_path:
         print(f"Shortest path from {start_node} to {end_node}: {' -> '.join(shortest_path)}")
+        shortest_path_distances = [str(graph[node].neighbors[next_node]) for node, next_node in zip(shortest_path[:-1], shortest_path[1:])]
+        total_weight = sum(map(int, shortest_path_distances))
+        print(f"Distances: {' + '.join(shortest_path_distances)} = {total_weight}")
+
+
+
     else:
         print(f"No path found from {start_node} to {end_node}")
 
